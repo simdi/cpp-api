c968ff5ee7ec955bdc597c4aa860af09d6508956
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-02-12 06:28:40 +0100

    Trying to serve static files.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,59 @@
 #include "crow_all.h"
 
+void sendFile(crow::response &res, std::string filename, std::string contentType) {
+  std::ifstream input("../public/" + filename, std::ifstream::in);
+
+  if (input) {
+    std::ostringstream contents;
+    contents << input.rdbuf();
+    input.close();
+    std::cout << contents.str() << std::endl;
+    res.set_header("Content-Type", contentType);
+    res.write(contents.str());
+  } else {
+    res.code = 404;
+    res.write("Not something Found");
+  }
+  res.end();
+}
+
+void sendHtml(crow::response &res, std::string filename) {
+  sendFile(res, filename + ".html", "text/html");
+}
+
+void sendImages(crow::response &res, std::string filename) {
+  sendFile(res, filename + "images/" + filename, "images/jpeg");
+}
+void sendScripts(crow::response &res, std::string filename) {
+  sendFile(res, filename + "scripts/" + filename, "text/javascript");
+}
+
+void sendStyles(crow::response &res, std::string filename) {
+  sendFile(res, filename + "styles/" + filename, "text/css");
+}
+
 int main(int argc, char* argv[]) {
+  std::cout << "argc: " << argc << " argv: " << argv << std::endl;
   crow::SimpleApp app;
+  // crow::mustache::set_base(".");
+
+  CROW_ROUTE(app, "/styles/<string>")([](const crow::request &req, crow::response &res, std::string filename){
+    sendStyles(res, filename);
+  });
+  
+  CROW_ROUTE(app, "/scripts/<string>")([](const crow::request &req, crow::response &res, std::string filename){
+    sendScripts(res, filename);
+  });
+
+  CROW_ROUTE(app, "/images/<string>")([](const crow::request &req, crow::response &res, std::string filename){
+    sendImages(res, filename);
+  });
 
   CROW_ROUTE(app, "/")([](const crow::request &req, crow::response &res){
-    std::ifstream in("../public/index.html", std::ifstream::in);
-    // if (in) {
-      std::ostringstream contents;
-      contents << in.rdbuf();
-      in.close();
-      std::cout << contents.str() << std::endl;
-      res.write(contents.str());
-    // } else {
-    //   res.write("Not something Found");
-    // }
-    res.end();
+    sendHtml(res, "index");
+    // crow::mustache::context ctx;
+    // auto main_page = crow::mustache::load("main.html");
+    // return main_page.render();
   });
 
   char* port = getenv("PORT");
